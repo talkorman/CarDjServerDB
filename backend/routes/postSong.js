3e59936d644e7f42a3914fbc4e9b4461d876c512
talkorman <talkorman1@gmail.com>
2021-07-05 01:47:57 +0800

    second commit

--- backend/routes/postSong.js
+++ backend/routes/postSong.js
@@ -3,30 +3,25 @@ const router = express.Router();
 const Post = require('../models/postSong');
 const bodyParser = require('body-parser');
 
-let avoidMultipleReq = '';
+let currentSearchWord = '';
 
 router.get("/:songDetail", (req, res, next) => {   
     let songsList = new Set();
-    const songs = req.params.songDetail;
-    if(songs != avoidMultipleReq){
-    setTimeout(()=>{
-        avoidMultipleReq = '';
-    }, 1000);
-}
-    if(avoidMultipleReq != songs){
-        res.writeHead(200,{'Content-Type': 'application/json'});
-        res.write(JSON.stringify({items: [{
-            videoId: "lkjlkjj",
-            title: "multiple result",
-            description: " ",
-            photoUrl: "kjhkjhkjhkjh"
-        }]}));
-        res.end();
-       
-    } 
-    avoidMultipleReq = songs;
+    const searchWord = req.params.songDetail;    
+    const reqTime = setInterval(()=>{
+    if(searchWord != currentSearchWord)
+    currentSearchWord = "";
+    }, 10000);
+    if(
+        searchWord != currentSearchWord
+        && searchWord != "gaocc" 
+        && searchWord != "favicon.ico" 
+        && searchWord != "guu4" 
+        && searchWord != "1" 
+        && searchWord.length > 2){
+    currentSearchWord = searchWord;
     const reg = /\+/g;
-    const songData = songs.replace(reg, ' ');
+    const songData = searchWord.replace(reg, ' ');
     //console.log(songData);
     Post.find({title: {$regex : songData, $options: 'i'}}, {_id: 0, __v: 0}).then(results => {
        for(let i = 0; i < results.length; i++){
@@ -39,5 +34,6 @@ router.get("/:songDetail", (req, res, next) => {
 res.write(JSON.stringify({items: songs}));
 res.end();
     })
+}
 })
     module.exports = router;
