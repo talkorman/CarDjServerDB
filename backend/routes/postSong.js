449a103abbd5a783b5ba02405ab57d2b63d6caef
talkorman <talkorman1@gmail.com>
2021-07-04 19:05:36 +0800

    First commit

---
+++ backend/routes/postSong.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/postSong');
+const bodyParser = require('body-parser');
+
+router.get("/:songDetail", (req, res, next) => {
+    let songsList = new Set();
+    const songs = req.params.songDetail;
+    const songData = songs.replaceAll('+', ' ');
+    console.log(songData);
+    Post.find({title: {$regex : songData, $options: 'i'}}, {_id: 0}).then(results => {
+       for(let i = 0; i < results.length; i++){
+           if(i < 11)
+           songsList.add(results[i]);
+       }
+       console.log(songsList);
+       res.json({
+           item: songsList
+       })
+    })
+})
+    module.exports = router;
