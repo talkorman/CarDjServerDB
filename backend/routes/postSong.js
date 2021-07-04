0be967dbdcf51d48d60bc522572aa92f9030aa51
talkorman <talkorman1@gmail.com>
2021-07-04 23:54:33 +0800

    second commit

--- backend/routes/postSong.js
+++ backend/routes/postSong.js
@@ -5,28 +5,26 @@ const bodyParser = require('body-parser');
 
 let avoidMultipleReq = '';
 
-router.get("/:songDetail", (req, res, next) => {
-    setTimeout(()=>{
-        if(avoidMultipleReq != '')
-        avoidMultipleReq = ''
-    }, 200);
+router.get("/:songDetail", (req, res, next) => {   
     let songsList = new Set();
     const songs = req.params.songDetail;
-    if(avoidMultipleReq == songs){
+    if(songs != avoidMultipleReq){
+    setTimeout(()=>{
         avoidMultipleReq = '';
-        return;
-    }
+    }, 5000);
+}
+    if(avoidMultipleReq == songs) return;
     avoidMultipleReq = songs;
     const reg = /\+/g;
     const songData = songs.replace(reg, ' ');
-    console.log(songData);
+    //console.log(songData);
     Post.find({title: {$regex : songData, $options: 'i'}}, {_id: 0, __v: 0}).then(results => {
        for(let i = 0; i < results.length; i++){
            if(i < 11)
            songsList.add(results[i]);
        }
        const songs = Array.from(songsList);
-       console.log(songs);
+       //console.log(songs);
        res.writeHead(200,{'Content-Type': 'application/json'});
 res.write(JSON.stringify({items: songs}));
 res.end();
